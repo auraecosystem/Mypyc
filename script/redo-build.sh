f0218ae151e497595ee1fbb4f5416f033d01d2ac
Seriki Walter Yakub <sandraisom@icloud.com>
2026-06-27 08:11:23 +0100

    Update redo-build.sh

--- script/redo-build.sh
+++ script/redo-build.sh
@@ -2,8 +2,8 @@
 git fetch --tags
 TAG=$(git tag | grep "$1")
 git checkout -b redo-$TAG $TAG
-git rebase master
+git rebase main
 git tag -f $TAG
 git push -f origin $TAG
-git checkout master
+git checkout main
 git branch -D redo-$TAG
